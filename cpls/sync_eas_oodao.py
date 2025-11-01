6b032b472380d8c73cd783948304a3e1477161b2
Jeff <jeff@voteagora.com>
2025-11-01 18:06:39 -0400

    undo

--- cpls/sync_eas_oodao.py
+++ cpls/sync_eas_oodao.py
@@ -26,7 +26,7 @@ class EASOoDaoSync(Sync):
 
     def __init__(self, infra_dao_slug, config=None, reset=False):
 
-        super().__init__(self, infra_dao_slug, config, reset)
+        super().__init__(infra_dao_slug, config, reset)
 
         try:
 
