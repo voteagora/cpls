87b34c4da79784dda03833d65c89ed1a94ea4b2b
Jeff <jeff@voteagora.com>
2025-11-03 14:53:34 -0500

    Redploy mainnet config, pick up config

--- cpls/sync_eas_oodao.py
+++ cpls/sync_eas_oodao.py
@@ -5,19 +5,35 @@ from .sync import Sync, SkipProposal, FIVE_MINUTES_IN_SECONDS, to_eth_address
 from .gcs import GCSClient
 
 OODAO = {
-   'INSTANTIATE' : '0x6bd280a85f895b15798d2b8e524a651e034f42b3ce614cb394c9d5e2ae2b10c7',
-   'PERMA_INSTANTIATE' : '0x01a6e34a0b986043b892902536c69a24f37fcfdaea39fbd2216dedbc6d53d83f',
-   'GRANT' : '0x7e4752a595f69560a5759e1acc1c70995758b45f00972374de9e1b801d19b758',
-   'CREATE_PROPOSAL_TYPE' : '0x880613f78650605d0a0617ec006b8181de80f688a12da6e78f1a8bf3d6b4f922',
-   'CREATE_PROPOSAL' : '0x442d586d8424b5485de1ff46cb235dcb96b41d19834926bbad1cd157fbeeb8fc',
-   'CHECK_PROPOSAL' : '0xd0fa030b9d06e954b910a86eeffc02aa641eaeef4216f9402ab4503f44c8e6a8',
-   'SET_PROPOSAL_TYPE' : '0x4468df37e17deb20b5096fb12107d4841b79ff6a62292e798eb7b79d0e764eb5',
-   'SET_PARAM_VALUE' : '0x7ac5f4a1c2c47e910132a546af770c1a6ff0c02e931020de6f955cf42eae9a6b',
-   'DELEGATED_SIMPLE_VOTE' : '0xde80f2c4e6168c2f68c1b466087ffba7994c2b7ff8f4113689c75ee82ef59c61',
-   'DELEGATED_ADVANCED_VOTE' : '0x4aa210b34a3b488c54f7ec482763c5ec8a52be5669c24216d3814b009076fb50',
-   'SIMPLE_VOTE' : '0x2b0e624e00310c7e88a1b7840238e285152b38ab00160b14c0d4e54e0a53a3aa',
-   'ADVANCED_VOTE' : '0xa7497737b4bdc0eaf60e90a290602216fb2a0e8c886e50bad63324ca8b76a587',
-   'DELETE' : '0x42793d748cbdd8d815556d7bbeacae5e82cbce605ea048474e7e60a02577cf49',
+    11155111 : {
+        'INSTANTIATE' : '0x6bd280a85f895b15798d2b8e524a651e034f42b3ce614cb394c9d5e2ae2b10c7',
+        'PERMA_INSTANTIATE' : '0x01a6e34a0b986043b892902536c69a24f37fcfdaea39fbd2216dedbc6d53d83f',
+        'GRANT' : '0x7e4752a595f69560a5759e1acc1c70995758b45f00972374de9e1b801d19b758',
+        'CREATE_PROPOSAL_TYPE' : '0x880613f78650605d0a0617ec006b8181de80f688a12da6e78f1a8bf3d6b4f922',
+        'CREATE_PROPOSAL' : '0x442d586d8424b5485de1ff46cb235dcb96b41d19834926bbad1cd157fbeeb8fc',
+        'CHECK_PROPOSAL' : '0xd0fa030b9d06e954b910a86eeffc02aa641eaeef4216f9402ab4503f44c8e6a8',
+        'SET_PROPOSAL_TYPE' : '0x4468df37e17deb20b5096fb12107d4841b79ff6a62292e798eb7b79d0e764eb5',
+        'SET_PARAM_VALUE' : '0x7ac5f4a1c2c47e910132a546af770c1a6ff0c02e931020de6f955cf42eae9a6b',
+        'DELEGATED_SIMPLE_VOTE' : '0xde80f2c4e6168c2f68c1b466087ffba7994c2b7ff8f4113689c75ee82ef59c61',
+        'DELEGATED_ADVANCED_VOTE' : '0x4aa210b34a3b488c54f7ec482763c5ec8a52be5669c24216d3814b009076fb50',
+        'SIMPLE_VOTE' : '0x2b0e624e00310c7e88a1b7840238e285152b38ab00160b14c0d4e54e0a53a3aa',
+        'ADVANCED_VOTE' : '0xa7497737b4bdc0eaf60e90a290602216fb2a0e8c886e50bad63324ca8b76a587',
+        'DELETE' : '0x42793d748cbdd8d815556d7bbeacae5e82cbce605ea048474e7e60a02577cf49',
+}, 1 : {
+    'INSTANTIATE' : '0x4ca31403392556d861e0d1b52eff1617a33128de3a5f479745b6c8556100f65e',
+    'PERMA_INSTANTIATE' : '0x9d13a1c5cc8ddf2a08fa83c2450703aca6f17d4db86caff5982ce3b20b9f1d2a',
+    'GRANT' : '0x366e5dd3956e245e20a87c7048e8254cf7d024d3d47c0a872a50d0020b77a75f',
+    'CREATE_PROPOSAL_TYPE' : '0x13194d67b1c5d22f8ea698bc37b0b376fcd35cc1d4baa43aebb99c30d69b6edb',
+    'CREATE_PROPOSAL' : '0x442d586d8424b5485de1ff46cb235dcb96b41d19834926bbad1cd157fbeeb8fc',
+    'CHECK_PROPOSAL' : '0x675048a4668d59d117cdbc5810912a1da791eb927fc8986ddbacccb4473d8b30',
+    'SET_PROPOSAL_TYPE' : '0x5e302e7f8743e8369750afaf8a822d6ca4ecbee78b737c4276220732c4488d7a',
+    'SET_PARAM_VALUE' : '0x23c692259d71fb2aa717353252e86560299fdeba8a3c839d808839d8eae5e17a',
+    'DELEGATED_SIMPLE_VOTE' : '0x4a0cdb428e86fa169d4d619ac22f0a7184ff3f63f02abca5ed059319646d0e43',
+    'DELEGATED_ADVANCED_VOTE' : '0x7cddf052e2011a22bdbcf2004655ca25b006a8e0955e0d537515907a94b88b1e',
+    'SIMPLE_VOTE' : '0xa6abd1e115de8e83b71f6db6db22d44d730a217f21e6008034f94e682efe9c70',
+    'ADVANCED_VOTE' : '0x4baeed1a31235fddb0757c6b0e006fee241019cd0ed3e0706ec2b2d1329acfe4',
+    'DELETE' : '0x3dd1714d33cc8dc91f24307758d4309a54a3b17080a3059bc67585e97a769a1e',
+    }
 }
 
 class EASOoDaoSync(Sync):
@@ -32,6 +48,7 @@ class EASOoDaoSync(Sync):
 
             self.oodao_dao_id = self.config['deployment']['oodao']['address']
             self.oodao_chain_id = self.config['deployment']['oodao']['chain_id']
+            self.oodao_shemas = OODAO[int(config['deployment']['oodao']['chain_id'])]
 
             self.token_addr = self.config['deployment']['token']['address']
             self.token_chain_id = self.config['deployment']['chain_id']
@@ -53,7 +70,7 @@ class EASOoDaoSync(Sync):
         pool = await self.pg.connect()
         async with pool.acquire() as connection:
             # No need to contract scope this, because the proposal_id is unique
-            qry = f"""select * from auazure."eas_attestations_v2" ocv WHERE topic3 in ('{OODAO['SIMPLE_VOTE']}', '{OODAO['ADVANCED_VOTE']}') and decoded_attestation->'proposal_id' = '{proposal_id}';"""
+            qry = f"""select * from auazure."eas_attestations_v2" ocv WHERE topic3 in ('{self.oodao_shemas['SIMPLE_VOTE']}', '{self.oodao_shemas['ADVANCED_VOTE']}') and decoded_attestation->'proposal_id' = '{proposal_id}';"""
             rows = await connection.fetch(qry)
             return rows
 
@@ -164,7 +181,7 @@ class EASOoDaoSync(Sync):
                         FROM 
                             auazure.eas_attestations_v2 eav
                         WHERE 
-                            topic3 = '{OODAO['SET_PROPOSAL_TYPE']}'
+                            topic3 = '{self.oodao_shemas['SET_PROPOSAL_TYPE']}'
                             AND decoded_attestation->>'proposal_id' = '{proposal_id}'
                         ORDER BY attestation_time desc limit 1
                         );"""
@@ -199,7 +216,7 @@ class EASOoDaoSync(Sync):
                                                 decoded_attestation->'startts' as startts,
                                                 decoded_attestation->>'description' as description,
                                                 data as proposal_id
-                                                from auazure."eas_attestations_v2" ocp WHERE topic3 = '{OODAO['CREATE_PROPOSAL']}' and topic1_cropped = '{self.oodao_dao_id}';""")
+                                                from auazure."eas_attestations_v2" ocp WHERE topic3 = '{self.oodao_shemas['CREATE_PROPOSAL']}' and topic1_cropped = '{self.oodao_dao_id}';""")
             return rows
 
     async def read_proposal_deletions(self):
