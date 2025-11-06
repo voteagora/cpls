3d34801fee9f8221794c7584b062f20d727fb234
Jeff <jeff@voteagora.com>
2025-11-05 22:11:25 -0500

    Fix existence check

--- cpls/sync.py
+++ cpls/sync.py
@@ -144,16 +144,13 @@ class Sync:
 
     async def read_existing_raw_proposal_hash_if_exists(self, proposal_id, gcs_client: 'GCSClient'):
 
-        blob_name = self.proposal_blob_name(proposal_id)
+        blob_name = self.proposal_blob_name(proposal_id) + ".gz"
 
         blob = await gcs_client.get_blob(blob_name)
-        print(f"Checking if {blob.name} exists...")
         try:
             exists = blob.exists()
-            print(exists)
         except Exception as e:
             msg = "Existence check failed, we can't proceed, we're blind.  We don't want to corrupt in case of the source pruning."
-            print(e)
             raise SkipProposal(msg, proposal_id=proposal_id)
 
         if exists:
