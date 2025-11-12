2f52f72717c8714024fef297390af1ea953a35d3
Jeff <jeff@voteagora.com>
2025-11-11 23:26:19 -0500

    Fix def

--- cpls/jobs.py
+++ cpls/jobs.py
@@ -40,7 +40,7 @@ class JobRequest(BaseModel):
     type: str
     payload: Dict
 
-async def create_http_client():
+def create_http_client():
     timeout = httpx.Timeout(connect=10.0, write=10.0, read=30.0)
     limits = httpx.Limits(max_connections=50, max_keepalive_connections=20)
     return httpx.AsyncClient(timeout=timeout, limits=limits)
