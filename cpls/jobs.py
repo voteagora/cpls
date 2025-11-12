a7da8d6bbf0f1d94a3da29bf2f920e5aae65a669
Jeff <jeff@voteagora.com>
2025-11-11 23:31:11 -0500

    Tweak timeouts

--- cpls/jobs.py
+++ cpls/jobs.py
@@ -41,8 +41,8 @@ class JobRequest(BaseModel):
     payload: Dict
 
 def create_http_client():
-    timeout = httpx.Timeout(connect=10.0, write=10.0, read=30.0)
-    limits = httpx.Limits(max_connections=50, max_keepalive_connections=20)
+    timeout = httpx.Timeout(15, connect=15)
+    limits = httpx.Limits(max_connections=15 * 6, max_keepalive_connections=20)
     return httpx.AsyncClient(timeout=timeout, limits=limits)
 
 class JobQueue:
