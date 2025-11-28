fab8febc68560118459aeb14162951638d3da7c4
Jeff <jeff@voteagora.com>
2025-11-28 08:26:47 -0500

    Revert >5 crop

--- cpls/sync_daonode.py
+++ cpls/sync_daonode.py
@@ -156,9 +156,6 @@ class DaoNodeSync(Sync):
         refreshed_count = 0
 
         for i, proposal_info in enumerate(proposals):
-
-            if i > 5:
-                break
         
             proposal_id = proposal_info['id']
 
